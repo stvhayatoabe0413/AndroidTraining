c9510132c84cd292e1f37dd242a6b927891f7c4c
stvhayatoabe0413 <eco,kurimatatoissyo@gmail.com>
2018-05-12 14:05:38 +0900

    Question1 完了_1

--- Question1/app/src/main/java/com/example/abehayato/question1/MainActivity.java
+++ Question1/app/src/main/java/com/example/abehayato/question1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.abehayato.question1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.logging.Logger;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a = 4;
+        // 変数の定義
+        boolean b = false;
+        String s = "文字列";
+        int n = 0;
+        double d = 0.0;
 
-        
+        // ログの出力
+        Logger logger = Logger.getLogger("MainActivity"); // 引数の文字列は通常クラス名にするものらしい。
+        logger.info("Boolean = " + b);
+        logger.info("String = " + s);
+        logger.info("Int = " + n);
+        logger.info("Double = " + d);
     }
 }
