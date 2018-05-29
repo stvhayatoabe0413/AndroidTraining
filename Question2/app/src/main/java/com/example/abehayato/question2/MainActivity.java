02727e550d46dbfe99909633e303a408f9f44126
stvhayatoabe0413 <eco,kurimatatoissyo@gmail.com>
2018-05-29 11:34:35 +0900

    5/29 コレクションの中身を追加。 Logger → Log に修正。

--- Question2/app/src/main/java/com/example/abehayato/question2/MainActivity.java
+++ Question2/app/src/main/java/com/example/abehayato/question2/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.abehayato.question2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.logging.Logger;
 
-// ArrayList型、HashMap型、HashSet型の変数を定義し、コンソールにログを出力する。(Log)
+// ArrayList型、HashMap型、HashSet型の変数を定義し、
+// いくつかの中身を入れ、中身をそれぞれコンソールにログで出力する。(Log)
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,14 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 変数の定義
-        ArrayList arrayList = new ArrayList();
-        HashMap hashMap = new HashMap();
-        HashSet hashSet = new HashSet();
+        ArrayList<String> arrayList = new ArrayList();
+        HashMap<Integer,String> hashMap = new HashMap();
+        HashSet<String> hashSet = new HashSet();
+
+        // 変数への代入
+        String[] strings = new String[]{"1","2","3"};
+        Arrays.stream(strings).forEach(s -> arrayList.add(s));
+        int[] keys = new int[]{0};   // keyとして上書きしながら使う配列。
+        Arrays.stream(strings).forEach(s -> hashMap.put(keys[0]++,s));
+        Arrays.stream(strings).forEach(s -> hashSet.add(s));
 
         // ログを出力
-        Logger logger = Logger.getLogger("Main");
-        logger.info("ArrayList = " + arrayList);
-        logger.info("HashMap = " + hashMap);
-        logger.info("HashSet = " + hashSet);
+//        Logger logger = Logger.getLogger("Main");
+//        logger.info("ArrayList = " + arrayList);
+//        logger.info("HashMap = " + hashMap);
+//        logger.info("HashSet = " + hashSet);
+        // Logger から Log に変更
+        arrayList.forEach(s -> Log.d("ArrayList ",s));
+        hashMap.forEach((key,value) -> Log.d("HashMap ",value));
+        hashSet.forEach(s -> Log.d("HashSet ",s));
     }
 }
