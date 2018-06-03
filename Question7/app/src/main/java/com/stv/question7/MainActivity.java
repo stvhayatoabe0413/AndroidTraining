1a0cbd94ffb743320040f3f7d9fb137bcbf8b5cd
stvhayatoabe0413 <eco,kurimatatoissyo@gmail.com>
2018-06-03 12:22:15 +0900

    Question7 Call Back (interface)

---
+++ Question7/app/src/main/java/com/stv/question7/MainActivity.java
@@ -0,0 +1,24 @@
+package com.stv.question7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+* 簡単な a+b を作成する。
+*
+* 。Result　インタゲースを定義する。
+* 【result(int resultValue) メソッド】
+* 。Mathクラスを定義する。
+* 【calculate() メソッド】
+* a+bの計算を実施する。結果をResultにパスする。
+* 
+* mainでResultインタフェースをinplementsし、Mathクラスのcalculate()メソッドを呼ぶと計算結果をLogに表示する。
+* */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
