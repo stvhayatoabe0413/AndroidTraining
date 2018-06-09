4f7f045c3839de6379b30cc88acae47325608fa8
stvhayatoabe0413 <eco,kurimatatoissyo@gmail.com>
2018-06-09 12:58:05 +0900

    Question9 ImageView

---
+++ Question9/app/src/main/java/com/stv/question9/MainActivity.java
@@ -0,0 +1,20 @@
+package com.stv.question9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+* 任意の画像を表示する。
+* なお、画像をdrawableフォルダーに格納すること。
+* ただし、下記の要件を満たすこと：
+* ・アスペクト比を保った状態(Aspect	Fit)
+* */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
